b39a14f3a015361e1851f2b078c882776d069a15
farman17 <69154746+farman17@users.noreply.github.com>
2020-10-10 22:41:03 +0700

    update jumbotron

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-8">
-                        <h1>Apa Kareba,Daengs!</h1>
+                        <h1>salamaki ta fada salama',Daengs!</h1>
                         <p>Informasi tentang bugis ada disini..salamaki</p>
                     </div>
                     <div class="col-md-4">
